d30d8e442c6b009991e9ce1f154c186b6d0196a3
Will Rynearson <will@developmentseed.org>
2025-08-20 16:54:10 +0200

    Update gh actions script

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/eo-predictor/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
